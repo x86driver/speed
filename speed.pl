94f64374b2d8284d2ccd6f1cc1545db3d1ae792b
root <root@doremi-desktop.garmin.com.tw>
2010-06-07 09:07:20 +0800

    增加可以切換的選項:
    performance or ondemand

--- speed.pl
+++ speed.pl
@@ -4,15 +4,20 @@ use strict;
 
 my $str;
 
-if (@ARGV != 1) {
-	print "Usage: speed.pl [Number of CPU]\n";
+if (@ARGV != 2) {
+	print "Usage: speed.pl [Number of CPU] [1: performance 2: ondemand]\n";
 	exit 0;
 }
 
 for (0..$ARGV[0]-1) {
 	$str = sprintf("/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor", $_);
-	print $str, "\n";
 	open F, ">$str";
-	print F "performance";
+	if ($ARGV[1] == 1) {
+		print F "performance";
+		printf "Set CPU$_: performance\n";
+	} else {
+		print F "ondemand";
+		printf "Set CPU$_: ondemand\n";
+	}
 	close F;
 }
